d1665c05fcc5f058aa56291148e684369a5a02c7
jgruber <john.t.gruber@gmail.com>
2019-08-15 16:07:00 -0500

    clean up

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ alsactl nrestore
 cp ./alsaloop/config/alsaloop.conf /etc/alsaloop.conf
 cp ./alsaloop/systemd/alsaloop.service /lib/systemd/system/alsaloop.service
 systemctl enable alsaloop.service
-systemclt alsaloop start
+systemctl alsaloop start
 
 # add KHConf SIP client service scripts and template configuration
 mkdir -p /var/lib/khconf
@@ -36,3 +36,21 @@ mkdir -p /var/lib/khstreamer/config
 cp ./khstreamer/config/template.conf /var/lib/khstreamer/config/template.conf
 
 # add meeting scheduler service
+mkdir -p /var/lib/meeting_scheduler
+cp ./meeting_scheduler/scripts/*.py /var/lib/meeting_scheduler/
+cp ./meeting_scheduler/systemd/meeting_scheduler.service /lib/systemd/system/meeting_scheduler.service
+systemctl enable meeting_scheduler.service
+systemctl start meeting_scheduler.service
+
+# add the configuration Web interface
+mkdir -p /var/lib/configuration_app
+cp ./configuration_app/app.py /var/lib/configuration_app/app.py
+mkdir -p /var/lib/configuration_app/templates
+cp ./configuration_app/configuration_app/templates/status.html /var/lib/configuration_app/templates/status.html
+cp ./configuration_app/configuration_app/templates/config.html /var/lib/configuration_app/templates/config.html
+mkdir -p /var/lib/configuration_app/static/stylesheets
+cp ./configuration_app/static/stylesheets/app.css /var/lib/configuration_app/static/stylesheets/app.css
+cp ./configuration_app/systemd/khaudioconfig.service /lib/systemd/system/khaudioconfig.service
+systemctl enable khaudioconfig.service
+systemctl start khaudioconfig.service
+
